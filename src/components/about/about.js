50692bcfcd7d0d91e177b171d7a964e76fce95d7
Byron Griffith <byrongriffith91@gmail.com>
2018-04-12 14:56:50 -0400

    about component adjusted

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
+import { Col, Row } from 'reactstrap';
+
 import './about.css';
 
 export default class About extends Component {
 	render() {
 		return(
-			<div class="mainDiv">
-				<br/>
-				<div class="row">
-					<div class="col-sm-12 col-md-12 col-lg-12">
-						<h2><strong>About/ Life Story</strong></h2>
-					</div>					
-				</div>
-		  		<br/>
-		  		<div class="row">
-					<div class="col-sm-12 col-md-12 col-lg-12">
-						<img class="aboutImg" src={"https://pixabay.com/en/avatar-icon-placeholder-1577909/"} alt="My Picture"/> 
-						<p class="aboutPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					</div>					
-				</div>
+			<div className="mainDiv">
+				<Row>
+					<Col sm="12" md="12" lg="12">
+						<h2><strong>About/ Life Story</strong></h2>	
+					</Col>
+				  </Row>
+
+				  <Row>
+					<Col sm="12" md="12" lg="3">
+						<img className="aboutImg" src="http://via.placeholder.com/300" alt="My Picture" /> 
+					</Col>
+					<Col sm="12" md="12" lg="9">
+						<p className="aboutPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</Col>					
+				</Row>
 			</div>
 		);
 	}
